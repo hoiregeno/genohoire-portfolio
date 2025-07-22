fbd204eb454089e4c1d0d2b715834e209d633caa
hoiregeno <hoiregeno75@gmail.com>
2025-07-22 10:22:07 +1000

    feat: setup Home, About, Portfolio and Contact pages with basic layout

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <main>
+      <section>
+        <h1>Home</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut labore quam consequatur sit obcaecati sapiente ratione architecto molestiae, minus praesentium ducimus odio nihil at natus, debitis harum sed impedit non modi cum! Quas velit obcaecati quis, aliquid temporibus perspiciatis quidem sequi blanditiis veniam autem eos accusamus libero consequuntur cumque aperiam quo hic! Repellendus tempora commodi doloribus eveniet quibusdam officiis a fuga iure libero ex eos voluptate enim adipisci quo debitis nulla obcaecati eaque id nobis ut modi, ducimus tenetur excepturi repellat. Aut obcaecati minus ab laborum impedit, quia dignissimos, rem eveniet velit ipsum quo debitis error, earum facere soluta blanditiis!
+        </p>
+      </section>
+    </main>
+  )
+}
+
+export default Home
