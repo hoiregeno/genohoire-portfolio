fc04d0f7b078cc072f60a926ae54e3f8725b9c87
hoiregeno <hoiregeno75@gmail.com>
2025-07-22 10:49:54 +1000

    feat(Navbar): setup class names for brand name and nav list

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import { NavLink, Link } from "react-router-dom"
+import styles from '../styles/Navbar.module.css'
 
 function Navbar() {
   return (
-    <nav>
-      <Link to='/'>GenoHoireDev</Link>
+    <nav className={styles.navbar}>
+      <Link to='/' className={styles.brandName}>GenoHoireDev</Link>
 
-      <ul>
+      <ul className={styles.linkWrapper}>
         <li><NavLink to='/home'>Home</NavLink></li>
         <li><NavLink to='/about'>About</NavLink></li>
         <li><NavLink to='/portfolio'>Portfolio</NavLink></li>
