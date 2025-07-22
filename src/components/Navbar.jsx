91a8f664add18e57c00c1617d54f412308affb22
hoiregeno <hoiregeno75@gmail.com>
2025-07-22 11:52:13 +1000

    feat(Navbar): implement sidebar toggle logic

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,30 @@
 import { NavLink, Link } from "react-router-dom"
 import styles from '../styles/Navbar.module.css'
+import { useState } from "react"
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleSidebar(){
+    setIsOpen(prev => !prev);
+  }
+
   return (
     <nav className={styles.navbar}>
       <Link to='/' className={styles.brandName}>GenoHoireDev</Link>
-      <button className={styles.openSidebarBtn}>
+      <button
+        className={styles.openSidebarBtn}
+        onClick={toggleSidebar}
+      >
         <svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
       </button>
       
-      <ul className={styles.linkWrapper}>
+      <ul className={`${styles.linkWrapper} ${isOpen ? styles.open : ''}`}>
         <li>
-          <button className={styles.closeSidebarBtn}>
+          <button
+            className={styles.closeSidebarBtn}
+            onClick={toggleSidebar}
+          >
             <svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
           </button>
         </li>
