e80feb70d2eae67ea787a76d31d6f18d59fe4b41
hoiregeno <hoiregeno75@gmail.com>
2025-07-22 13:06:12 +1000

    chore(Navbar): organize code structure for better readability within a fragment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,30 +10,44 @@ function Navbar() {
   }
 
   return (
-    <nav className={styles.navbar}>
-      <Link to='/' className={styles.brandName}>GenoHoireDev</Link>
-      <button
-        className={styles.openSidebarBtn}
-        onClick={toggleSidebar}
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
-      </button>
+    <>
+      <nav className={styles.navbar}>
+        <Link to='/' className={styles.brandName}>GenoHoireDev</Link>
+        <button
+          className={styles.openSidebarBtn}
+          onClick={toggleSidebar}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px">
+            <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/>
+          </svg>
+        </button>
       
-      <ul className={`${styles.linkWrapper} ${isOpen ? styles.open : ''}`}>
-        <li>
-          <button
-            className={styles.closeSidebarBtn}
-            onClick={toggleSidebar}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
-          </button>
-        </li>
-        <li><NavLink to='/home'>Home</NavLink></li>
-        <li><NavLink to='/about'>About</NavLink></li>
-        <li><NavLink to='/portfolio'>Portfolio</NavLink></li>
-        <li><NavLink to='/contact'>Contact</NavLink></li>
-      </ul>
-    </nav>
+        <ul className={`${styles.linkWrapper} ${isOpen ? styles.open : ''}`}>
+          <li>
+            <button
+              className={styles.closeSidebarBtn}
+              onClick={toggleSidebar}
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px">
+                <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/>
+              </svg>
+            </button>
+          </li>
+          <li>
+            <NavLink to='/home' onClick={toggleSidebar}>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to='/about' onClick={toggleSidebar}>About</NavLink>
+          </li>
+          <li>
+            <NavLink to='/portfolio' onClick={toggleSidebar}>Portfolio</NavLink>
+          </li>
+          <li>
+            <NavLink to='/contact' onClick={toggleSidebar}>Contact</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </>
   )
 }
 
