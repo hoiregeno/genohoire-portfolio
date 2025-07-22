53db3df172649ad14dad7188528b9517ccf95b56
hoiregeno <hoiregeno75@gmail.com>
2025-07-22 11:30:06 +1000

    feat(Navbar): add sidebar toggle buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,15 @@ function Navbar() {
   return (
     <nav className={styles.navbar}>
       <Link to='/' className={styles.brandName}>GenoHoireDev</Link>
-
+      <button className={styles.openSidebarBtn}>
+        <svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
+      </button>
       <ul className={styles.linkWrapper}>
+        <li>
+          <button className={styles.closeSidebarBtn}>
+            <svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
+          </button>
+        </li>
         <li><NavLink to='/home'>Home</NavLink></li>
         <li><NavLink to='/about'>About</NavLink></li>
         <li><NavLink to='/portfolio'>Portfolio</NavLink></li>
